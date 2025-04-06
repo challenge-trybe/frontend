49014b5ea52c83c6ed86a37035445bf7289bc666
rnignon <rnignon@naver.com>
2025-04-06 18:42:44 +0900

    feat: 검색바와 알림 아이콘이 존재하는 SearchHeader 구현

---
+++ src/components/header/SearchHeader.tsx
@@ -0,0 +1,61 @@
+import {Pressable, StyleSheet, View} from 'react-native';
+import React, {useState} from 'react';
+import SearchBar from '../SearchBar';
+import Icon from '../Icon';
+import colors from '../../styles/colors';
+
+interface Props {
+  placeHolder?: string;
+  onPressSearch: () => void;
+  backgroundColor?: string;
+}
+
+const SearchHeader = ({
+  placeHolder,
+  onPressSearch,
+  backgroundColor = colors.white,
+}: Props) => {
+  const [keyword, setKeyword] = useState('');
+
+  const onPressNotification = () => {
+    // TODO: 로그인 상태 시 알림창으로, 로그인 상태가 아닐 시 로그인 창으로
+  };
+
+  return (
+    <View style={[styles.container, {backgroundColor}]}>
+      <View style={styles.searchBar}>
+        <SearchBar
+          value={keyword}
+          onChangeText={setKeyword}
+          onSubmit={onPressSearch}
+          onClear={() => setKeyword('')}
+          placeholder={placeHolder}
+        />
+      </View>
+      <Pressable
+        style={({pressed}) => ({
+          opacity: pressed ? 1 : 0.8,
+        })}
+        onPress={onPressNotification}>
+        <Icon name="notifications-outline" size={28} color={colors.gray600} />
+      </Pressable>
+    </View>
+  );
+};
+
+export default SearchHeader;
+
+const styles = StyleSheet.create({
+  container: {
+    height: 80,
+    paddingHorizontal: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+    borderBottomWidth: 1,
+    borderColor: colors.gray100,
+  },
+  searchBar: {
+    flex: 1,
+  },
+});
