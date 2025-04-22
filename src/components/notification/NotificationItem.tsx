7de778a525544294d36c53b26e92350ca2634b13
rnignon <rnignon@naver.com>
2025-04-23 00:10:17 +0900

    style: 알림 아이템 컴포넌트 읽음 상태에 따른 디자인 변경

--- src/components/notification/NotificationItem.tsx
+++ src/components/notification/NotificationItem.tsx
@@ -19,18 +19,30 @@ const NotificationItem = ({notification}: Props) => {
     // TODO: 삭제 API 호출
   };
 
+  const isRead = notification.isRead;
+
   return (
-    <TouchableOpacity style={styles.container} activeOpacity={0.7}>
+    <TouchableOpacity
+      style={[styles.container, isRead && styles.readContainer]}
+      activeOpacity={0.7}>
       <View style={styles.header}>
-        <Text style={styles.title}>{notification.title}</Text>
+        <Text style={[styles.title, isRead && styles.readText]}>
+          {notification.title}
+        </Text>
         <TouchableOpacity activeOpacity={0.7}>
-          <Icon name="close" size={24} color="gray" />
+          <Icon
+            name="close-outline"
+            size={30}
+            color={isRead ? colors.gray500 : colors.gray600}
+          />
         </TouchableOpacity>
       </View>
       <View style={styles.body}>
-        <Text>{notification.message}</Text>
+        <Text style={[styles.message, isRead && styles.readText]}>
+          {notification.message}
+        </Text>
         <FormattedDate
-          style={styles.timestamp}
+          style={[styles.timestamp, isRead && styles.readTimestamp]}
           type="relative"
           date={notification.timestamp}
         />
@@ -47,6 +59,9 @@ const styles = StyleSheet.create({
     backgroundColor: colors.white,
     gap: 8,
   },
+  readContainer: {
+    backgroundColor: colors.gray100,
+  },
   header: {
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -56,6 +71,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'flex-end',
+    gap: 12,
   },
   title: {
     fontSize: 16,
@@ -69,5 +85,11 @@ const styles = StyleSheet.create({
   timestamp: {
     fontSize: 12,
     color: colors.gray600,
+  },
+  readText: {
+    color: colors.gray600,
+  },
+  readTimestamp: {
+    color: colors.gray500,
   },
 });
