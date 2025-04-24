41adf29c5ead40b84540083ac2a032c018746a84
rnignon <rnignon@naver.com>
2025-04-24 20:26:12 +0900

    refactor: 컴포넌트 분리 후, 이를 사용하도록 코드 수정

--- src/components/challenge/ChallengeItem.tsx
+++ src/components/challenge/ChallengeItem.tsx
@@ -5,6 +5,9 @@ import Text from '../Text';
 import Icon from '../Icon';
 import colors from '../../styles/colors';
 import {getCategoryColors, getStatusColors} from './challengeColors';
+import Badge from '../common/Badge';
+import Bookmark from '../common/Bookmark';
+import ParticipantInfo from '../common/ParticipantInfo';
 
 type Props = {
   challenge: ChallengePreview;
@@ -15,16 +18,8 @@ type Props = {
 const ChallengeItem = ({challenge, onPress, onPressBookmark}: Props) => {
   const opacity = 0.9;
 
-  const bookmarkIconSize = 28;
-  const bookmarkIconColor = colors.red600;
-
   const categoryColor = getCategoryColors(challenge.category);
   const statusColor = getStatusColors(challenge.status);
-
-  const participantColor =
-    challenge.participantCount >= challenge.capacity
-      ? colors.gray500
-      : colors.gray700;
 
   return (
     <TouchableOpacity
@@ -37,63 +32,40 @@ const ChallengeItem = ({challenge, onPress, onPressBookmark}: Props) => {
           style={styles.thumbnail}
           resizeMode="cover"
         />
-        <View style={styles.bookmarkContainer}>
-          <TouchableOpacity
-            activeOpacity={opacity}
-            onPress={() =>
-              onPressBookmark(challenge.id, challenge.bookmark.bookmarked)
-            }>
-            {challenge.bookmark.bookmarked ? (
-              <Icon
-                name="bookmark"
-                size={bookmarkIconSize}
-                color={bookmarkIconColor}
-              />
-            ) : (
-              <Icon
-                name="bookmark-outline"
-                size={bookmarkIconSize}
-                color={bookmarkIconColor}
-              />
-            )}
-          </TouchableOpacity>
-          <Text style={styles.bookmarkCount}>
-            {challenge.bookmark.bookmarkCount}
-          </Text>
-        </View>
+        <Bookmark
+          bookmarked={challenge.bookmark.bookmarked}
+          count={challenge.bookmark.bookmarkCount}
+          onPress={() =>
+            onPressBookmark(challenge.id, challenge.bookmark.bookmarked)
+          }
+        />
       </View>
       <View style={styles.infoContainer}>
-        <Text
-          style={[
-            styles.category,
-            {
-              backgroundColor: categoryColor.backgroundColor,
-              color: categoryColor.textColor,
-            },
-          ]}>
+        <Badge
+          style={styles.category}
+          backgroundColor={categoryColor.backgroundColor}
+          color={categoryColor.textColor}>
           {challenge.category}
-        </Text>
+        </Badge>
         <Text numberOfLines={1}>{challenge.title}</Text>
         <Text style={styles.description} numberOfLines={3}>
           {challenge.description}
         </Text>
         <View style={styles.footerContainer}>
-          <View
-            style={[
-              styles.statusContainer,
-              {backgroundColor: statusColor.secondary},
-            ]}>
-            <Icon name="ellipse" size={10} color={statusColor.primary} />
-            <Text style={[styles.status, {color: statusColor.primary}]}>
-              {challenge.status}
-            </Text>
-          </View>
-          <View style={styles.participantContainer}>
-            <Icon name="person" size={12} color={participantColor} />
-            <Text style={[styles.participantCount, {color: participantColor}]}>
-              {challenge.participantCount}/{challenge.capacity}
-            </Text>
-          </View>
+          <Badge
+            backgroundColor={statusColor.secondary}
+            color={statusColor.primary}>
+            <View style={styles.statusContainer}>
+              <Icon name="ellipse" size={10} color={statusColor.primary} />
+              <Text style={[styles.status, {color: statusColor.primary}]}>
+                {challenge.status}
+              </Text>
+            </View>
+          </Badge>
+          <ParticipantInfo
+            participantCount={challenge.participantCount}
+            capacity={challenge.capacity}
+          />
         </View>
       </View>
     </TouchableOpacity>
@@ -121,29 +93,12 @@ const styles = StyleSheet.create({
     borderColor: colors.gray300,
     borderRadius: 6,
   },
-  bookmarkContainer: {
-    position: 'absolute',
-    top: 6,
-    right: 6,
-    alignItems: 'center',
-  },
-  bookmarkCount: {
-    color: colors.red600,
-    fontWeight: '900',
-    fontSize: 12,
-    textShadowColor: colors.white,
-    textShadowOffset: {width: 0, height: 0},
-    textShadowRadius: 10,
-  },
   infoContainer: {
     flex: 1,
     alignItems: 'flex-start',
     gap: 6,
   },
   category: {
-    paddingVertical: 2,
-    paddingHorizontal: 8,
-    borderRadius: 12,
     fontSize: 10,
   },
   description: {
@@ -160,23 +115,12 @@ const styles = StyleSheet.create({
     marginTop: 4,
   },
   statusContainer: {
-    paddingVertical: 2,
-    paddingHorizontal: 8,
-    borderRadius: 12,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
     gap: 2,
   },
   status: {
-    fontSize: 12,
-  },
-  participantContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 4,
-  },
-  participantCount: {
     fontSize: 12,
   },
 });
